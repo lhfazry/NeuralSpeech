312b8c91872000536122fe92920b386c5bbe14ee
Fazry <lhuqita.fazry@gmail.com>
2023-01-06 07:20:13 +0900

    d

--- PriorGrad-vocoder/learner.py
+++ PriorGrad-vocoder/learner.py
@@ -181,9 +181,9 @@ class PriorGradLearner:
             noise_scale = self.noise_level[t].unsqueeze(1)
             noise_scale_sqrt = noise_scale ** 0.5
             noise = torch.randn_like(audio)
-            print(f'noise shape: {noise.shape}')
+            #print(f'noise shape: {noise.shape}')
             noise = get_color_noise(N, T, audio.dtype, self.params.noise_color).to(device)
-            print(f'color noise shape: {noise.shape}')
+            #print(f'color noise shape: {noise.shape}')
             noise = noise * target_std
             noisy_audio = noise_scale_sqrt * audio + (1.0 - noise_scale) ** 0.5 * noise
 
