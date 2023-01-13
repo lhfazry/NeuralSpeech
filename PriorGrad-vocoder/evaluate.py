eb4dd7b83ac700e415b43e821554d4fbe3ca640d
Fazry <lhuqita.fazry@gmail.com>
2023-01-13 15:45:17 +0900

    d

--- PriorGrad-vocoder/evaluate.py
+++ PriorGrad-vocoder/evaluate.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 from nnmnkwii.metrics import melcd
 from argparse import ArgumentParser
+from preprocess import MAX_WAV_VALUE, get_mel, normalize
 from params import params
 from scipy.io.wavfile import read
 
@@ -17,22 +18,8 @@ def main(args):
     results = []
 
     for fname in os.listdir(args.sdir):
-        _, synthetic_wav = read(os.path.join(args.sdir, fname))
-        _, original_wav = read(os.path.join(args.odir, fname))
-
-        #synthetic_wav = synthetic_wav.astype(np.float64)
-        #f0_1, timeaxis_1 = pyworld.harvest(synthetic_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
-        #sp1 = pyworld.cheaptrick(synthetic_wav, f0_1, timeaxis_1, sr)  
-
-        #original_wav = original_wav.astype(np.float64)
-        #f0_2, timeaxis_2 = pyworld.harvest(original_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
-        #sp2 = pyworld.cheaptrick(original_wav, f0_2, timeaxis_2, sr)
-
-        # mel-cepstrum
-        #coded_sp_1 = pyworld.code_spectral_envelope(sp1, sr, 24)
-        #coded_sp_2 = pyworld.code_spectral_envelope(sp2, sr, 24)
-        synthetic_mels = get_mel(torch.tensor(synthetic_wav), params)
-        original_mels = get_mel(torch.tensor(original_wav), params)
+        synthetic_mels = load_mels(os.path.join(args.sdir, fname))
+        original_mels = load_mels(os.path.join(args.odir, fname))
 
         result = melcd(synthetic_mels.numpy(), original_mels.numpy() , lengths=None)
         print(result)
@@ -41,6 +28,24 @@ def main(args):
 
     print(results)
 
+def load_mels(audio_file):
+    sr, audio = read(audio_file)
+
+    if params.sample_rate != sr:
+        raise ValueError(f'Invalid sample rate {sr}.')
+
+    audio = audio / MAX_WAV_VALUE
+    audio = normalize(audio) * 0.95
+
+    # match audio length to self.hop_size * n for evaluation
+    if (audio.shape[0] % params.hop_samples) != 0:
+        audio = audio[:-(audio.shape[0] % params.hop_samples)]
+    
+    audio = torch.FloatTensor(audio)
+    spectrogram = get_mel(audio, params)
+
+    return spectrogram
+    
 if __name__ == '__main__':
     parser = ArgumentParser(description='Calculate MCD')
     parser.add_argument('--sdir', help='Synthetic directory of waveform')
