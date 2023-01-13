d4c30afd77132a391963992e5e30d04f24011c9e
Fazry <lhuqita.fazry@gmail.com>
2023-01-13 15:07:20 +0900

    d

--- PriorGrad-vocoder/evaluate.py
+++ PriorGrad-vocoder/evaluate.py
@@ -3,10 +3,11 @@ import subprocess
 import sys
 import glob
 import librosa
-import pyworld
+from preprocess import get_mel
 import numpy as np
 from nnmnkwii.metrics import melcd
 from argparse import ArgumentParser
+from params import params
 
 def main(args):
     sr = 16000
@@ -17,20 +18,23 @@ def main(args):
         synthetic_wav, _ = librosa.load(os.path.join(args.sdir, fname), sr=sr, mono=True)
         original_wav, _ = librosa.load(os.path.join(args.odir, fname), sr=sr, mono=True)
 
-        synthetic_wav = synthetic_wav.astype(np.float64)
-        f0_1, timeaxis_1 = pyworld.harvest(synthetic_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
-        sp1 = pyworld.cheaptrick(synthetic_wav, f0_1, timeaxis_1, sr)  
+        #synthetic_wav = synthetic_wav.astype(np.float64)
+        #f0_1, timeaxis_1 = pyworld.harvest(synthetic_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
+        #sp1 = pyworld.cheaptrick(synthetic_wav, f0_1, timeaxis_1, sr)  
 
-        original_wav = original_wav.astype(np.float64)
-        f0_2, timeaxis_2 = pyworld.harvest(original_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
-        sp2 = pyworld.cheaptrick(original_wav, f0_2, timeaxis_2, sr)
+        #original_wav = original_wav.astype(np.float64)
+        #f0_2, timeaxis_2 = pyworld.harvest(original_wav, sr, frame_period=5.0, f0_floor=71.0, f0_ceil=800.0)
+        #sp2 = pyworld.cheaptrick(original_wav, f0_2, timeaxis_2, sr)
 
         # mel-cepstrum
-        coded_sp_1 = pyworld.code_spectral_envelope(sp1, sr, 24)
-        coded_sp_2 = pyworld.code_spectral_envelope(sp2, sr, 24)
+        #coded_sp_1 = pyworld.code_spectral_envelope(sp1, sr, 24)
+        #coded_sp_2 = pyworld.code_spectral_envelope(sp2, sr, 24)
+        synthetic_mels = get_mel(synthetic_wav, params)
+        original_mels = get_mel(original_wav, params)
 
-        result = melcd(coded_sp_1,coded_sp_2 , lengths=None)
+        result = melcd(synthetic_mels, original_mels , lengths=None)
         print(result)
+        
         results.append(dict(fname=fname, score=result))
 
     print(results)
