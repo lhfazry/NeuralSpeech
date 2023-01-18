49aaaa5d6d988d85a8ce457723a9dcbe9b6a1905
Fazry <lhuqita.fazry@gmail.com>
2023-01-18 13:11:56 +0900

    d

--- PriorGrad-vocoder/evaluate.py
+++ PriorGrad-vocoder/evaluate.py
@@ -13,14 +13,12 @@ from params import params
 from scipy.io.wavfile import read
 
 def main(args):
-    sr = 16000
-
     results = 0
     total = 0
 
     for fname in os.listdir(args.sdir):
         synthetic_mels = load_mels(os.path.join(args.sdir, fname))
-        original_mels = load_mels(os.path.join(args.odir, fname), should_normalize=True)
+        original_mels = load_mels(os.path.join(args.odir, fname))
 
         result = melcd(synthetic_mels.numpy(), original_mels.numpy() , lengths=None)
         print(f"{fname} ==> {result}")
@@ -31,15 +29,14 @@ def main(args):
 
     print(f"average: {results/total}")
 
-def load_mels(audio_file, should_normalize=False):
+def load_mels(audio_file):
     sr, audio = read(audio_file)
 
     if params.sample_rate != sr:
         raise ValueError(f'Invalid sample rate {sr}.')
 
-    if should_normalize:
-        audio = audio / MAX_WAV_VALUE
-        audio = normalize(audio) * 0.95
+    audio = audio / MAX_WAV_VALUE
+    audio = normalize(audio) * 0.95
 
     # match audio length to self.hop_size * n for evaluation
     if (audio.shape[0] % params.hop_samples) != 0:
