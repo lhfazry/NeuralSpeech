ac6e62445fcb092f4fc897eca0d416b5056ccc48
Fazry <lhuqita.fazry@gmail.com>
2023-01-20 14:01:19 +0900

    add new config

--- PriorGrad-vocoder/params.py
+++ PriorGrad-vocoder/params.py
@@ -89,7 +89,7 @@ params = AttrDict(
     noise_dist=1, #1=gaussian, 2=gamma
     gamma_init_scale=0.001,
 
-    use_glot=True,
+    use_glot=False,
     use_mels=True,
 
     # accumulative prediction
